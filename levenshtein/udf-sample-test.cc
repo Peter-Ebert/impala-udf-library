5cea15bc8285a8c1b308d07ad49bf3abc3839d99
peter-ebert <6597540+Peter-Ebert@users.noreply.github.com>
2019-10-09 13:28:32 -0700

    function test2

--- levenshtein/udf-sample-test.cc
+++ levenshtein/udf-sample-test.cc
@@ -25,33 +25,12 @@ int main(int argc, char** argv) {
   bool passed = true;
   // Using the test harness helpers, validate the UDF returns correct results.
   // These tests validate:
-  //  AddUdf(1, 2) == 3
-  //  AddUdf(null, 2) == null
-  passed &= UdfTestHarness::ValidateUdf<IntVal, IntVal, IntVal>(
-      AddUdf, IntVal(1), IntVal(2), IntVal(3));
-  passed &= UdfTestHarness::ValidateUdf<IntVal, IntVal, IntVal>(
-      AddUdf, IntVal::null(), IntVal(2), IntVal::null());
+  
+  passed &= UdfTestHarness::ValidateUdf<StringVal, StringVal>(
+      CountVowels, StringVal("hello"), IntVal(2));
+  // passed &= UdfTestHarness::ValidateUdf<IntVal, IntVal, IntVal>(
+  //     AddUdf, IntVal::null(), IntVal(2), IntVal::null());
 
-  // Test FuzzyEquals sample.
-  passed &= UdfTestHarness::ValidateUdf<BooleanVal, DoubleVal, DoubleVal>(
-      FuzzyEquals, DoubleVal(1.0), DoubleVal(1.0000000001), BooleanVal(true));
-  passed &= UdfTestHarness::ValidateUdf<BooleanVal, DoubleVal, DoubleVal>(
-      FuzzyEquals, DoubleVal(1.1), DoubleVal(1.0), BooleanVal(false));
-
-  // Test ReturnConstantArg sample
-  // Test constant arg
-  vector<AnyVal*> constant_args;
-  constant_args.push_back(new IntVal(1));
-  passed &= UdfTestHarness::ValidateUdf<IntVal, IntVal>(
-      ReturnConstantArg, IntVal(1), IntVal(1), ReturnConstantArgPrepare,
-      ReturnConstantArgClose, constant_args);
-  delete constant_args[0];
-  constant_args.clear();
-
-  // Test non-constant arg
-  passed &= UdfTestHarness::ValidateUdf<IntVal, IntVal>(
-      ReturnConstantArg, IntVal(1), IntVal::null(), ReturnConstantArgPrepare,
-      ReturnConstantArgClose);
 
   cout << "Tests " << (passed ? "Passed." : "Failed.") << endl;
   return !passed;
